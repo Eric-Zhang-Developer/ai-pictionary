5dca6d4c7c89db1c24be61a215b11734b74a6baf
Eric Zhang <superezzhang@gmail.com>
2025-07-24 18:22:47 -0400

    feat: draw zone placeholder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ export default function Home() {
     <div className="flex items-center justify-center flex-col gap-4">
       <h1 className="text-4xl">AI Pictionary</h1>
       <h2 className="text-3xl">Draw a Cat</h2>
+      <div className="border-2 px-120 py-60 rounded-2xl">
+      </div>
 
 
     </div>
