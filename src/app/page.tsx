15397b8e7a2e9091edab65b73a7cc5bb49f01eff
Eric Zhang <superezzhang@gmail.com>
2025-07-24 18:19:13 -0400

    feat: title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 export default function Home() {
   return (
-    <div></div>
+    <div className="flex items-center justify-center">
+      <h1 className="text-3xl">AI Pictionary</h1>
+
+
+    </div>
   );
 }
