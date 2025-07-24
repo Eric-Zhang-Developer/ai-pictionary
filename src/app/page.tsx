6b6d203388f42d873473990a32edc500b53715ee
Eric Zhang <superezzhang@gmail.com>
2025-07-24 18:20:11 -0400

    feat: draw command

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <div className="flex items-center justify-center">
-      <h1 className="text-3xl">AI Pictionary</h1>
+    <div className="flex items-center justify-center flex-col gap-4">
+      <h1 className="text-4xl">AI Pictionary</h1>
+      <h2 className="text-3xl">Draw a Cat</h2>
 
 
     </div>
