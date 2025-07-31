2869c9087cd555a08865d1d30fb7a141150d03be
Eric Zhang <superezzhang@gmail.com>
2025-07-31 14:03:14 -0400

    feat: first Gemini API Call!!!!!

--- src/app/api/generate-response/route.ts
+++ src/app/api/generate-response/route.ts
@@ -1,12 +1,19 @@
 import { NextResponse } from "next/server";
+import { GoogleGenAI } from "@google/genai";
 
 export async function POST(request: Request) {
   try {
     const body = await request.json();
     const textPrompt = body.textPrompt;
 
+    const ai = new GoogleGenAI({});
+    const response = await ai.models.generateContent({
+      model: "gemini-2.5-flash",
+      contents: "This is my first API Call! Greetings!"
+    })
 
-    return NextResponse.json({ guess: textPrompt });
+
+    return NextResponse.json({ response: response.text });
   } catch (error) {
     return NextResponse.json({ error: error }, { status: 400 });
   }
