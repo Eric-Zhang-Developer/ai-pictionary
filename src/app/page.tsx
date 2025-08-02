7025e656134f382d88b5f708d221af25756051a5
Eric Zhang <superezzhang@gmail.com>
2025-08-02 11:43:47 -0400

    feat: set up new useState for guess correctness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 "use client"
 import Sketchpad from "@/components/Sketchpad";
+import { GuessState } from "@/utils/types";
 import { useState } from "react";
 
 export default function Home() {
   const [response, setResponse] = useState<string>("");
+  const [guessState, setGuessState] = useState<GuessState>(GuessState.Pending)
   // the image url processing happens entirely in Sketchpad. For now it is discarded after being given to the API. For future reference could save it. 
   return (
     <div className="flex items-center justify-center flex-col gap-4 container mx-auto">
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,3 +1,9 @@
 export interface SketchpadProps {
   setResponse: (response: string) => void;
 }
+
+export enum GuessState {
+  Pending = "PENDING",
+  Correct = "CORRECT",
+  Incorrect = "INCORRECT"
+}
